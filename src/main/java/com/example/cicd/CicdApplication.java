847104e94ed8eb48833d934d2016d2b7afa6ce6c
Keshav Kumar2 <keshav.kumar2@mphasis.com>
2023-05-22 17:08:50 +0530

    sop

--- src/main/java/com/example/cicd/CicdApplication.java
+++ src/main/java/com/example/cicd/CicdApplication.java
@@ -3,11 +3,16 @@ package com.example.cicd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class CicdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CicdApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(CicdApplication.class, args);
+		System.out.println("testing");
+
 	}
-    //aa
+
+    //aaa("Hello World");
 }
